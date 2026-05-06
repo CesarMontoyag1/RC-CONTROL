ab5af3f8eb8a48b8702dea7f84eed7fcb5c0dc0c
Cesar Montoya <159492748+CesarMontoyag1@users.noreply.github.com>
2026-05-05 20:45:46 -0500

    Add comment for Bluetooth connection initialization

--- RCControl_Android_Studio/app/src/main/java/com/example/rccontrol/MainActivity.java
+++ RCControl_Android_Studio/app/src/main/java/com/example/rccontrol/MainActivity.java
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Coneccion bluetooth
         initUI();
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
